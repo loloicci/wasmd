7aac26f37de8c87e797cc6cdd82e3b36d544a7a5
Daisuke Iuchi <42408108+da1suk8@users.noreply.github.com>
2023-04-21 18:59:56 +0900

    test: update on expected gas

--- x/wasm/keeper/keeper_test.go
+++ x/wasm/keeper/keeper_test.go
@@ -410,7 +410,7 @@ func TestInstantiate(t *testing.T) {
 
 	gasAfter := ctx.GasMeter().GasConsumed()
 	if types.EnableGasVerification {
-		require.Equal(t, uint64(0x194a1), gasAfter-gasBefore)
+		require.Equal(t, uint64(0x194a5), gasAfter-gasBefore)
 	}
 
 	// ensure it is stored properly
@@ -854,7 +854,7 @@ func TestExecute(t *testing.T) {
 	// make sure gas is properly deducted from ctx
 	gasAfter := ctx.GasMeter().GasConsumed()
 	if types.EnableGasVerification {
-		require.Equal(t, uint64(0x16c68), gasAfter-gasBefore)
+		require.Equal(t, uint64(0x16c6c), gasAfter-gasBefore)
 	}
 	// ensure bob now exists and got both payments released
 	bobAcct = accKeeper.GetAccount(ctx, bob)
